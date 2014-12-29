c36b5663c8da494ebf8c27a4b5eba85f9fd24981
lawrencely <lawrence.ly667@gmail.com>
2014-12-30 00:47:40 +1100

    routes and sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    customer = Customer.where(:name => params[:username]).first
+    if customer.present? && customer.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,71 @@
 # == Route Map
 #
-# You don't have any routes defined!
-#
-# Please add some routes in config/routes.rb.
-#
-# For more information about routes, see the Rails guide: http://guides.rubyonrails.org/routing.html.
+#          Prefix Verb   URI Pattern                     Controller#Action
+#      categories GET    /categories(.:format)           categories#index
+#                 POST   /categories(.:format)           categories#create
+#    new_category GET    /categories/new(.:format)       categories#new
+#   edit_category GET    /categories/:id/edit(.:format)  categories#edit
+#        category GET    /categories/:id(.:format)       categories#show
+#                 PATCH  /categories/:id(.:format)       categories#update
+#                 PUT    /categories/:id(.:format)       categories#update
+#                 DELETE /categories/:id(.:format)       categories#destroy
+#       customers GET    /customers(.:format)            customers#index
+#                 POST   /customers(.:format)            customers#create
+#    new_customer GET    /customers/new(.:format)        customers#new
+#   edit_customer GET    /customers/:id/edit(.:format)   customers#edit
+#        customer GET    /customers/:id(.:format)        customers#show
+#                 PATCH  /customers/:id(.:format)        customers#update
+#                 PUT    /customers/:id(.:format)        customers#update
+#                 DELETE /customers/:id(.:format)        customers#destroy
+#        products GET    /products(.:format)             products#index
+#                 POST   /products(.:format)             products#create
+#     new_product GET    /products/new(.:format)         products#new
+#    edit_product GET    /products/:id/edit(.:format)    products#edit
+#         product GET    /products/:id(.:format)         products#show
+#                 PATCH  /products/:id(.:format)         products#update
+#                 PUT    /products/:id(.:format)         products#update
+#                 DELETE /products/:id(.:format)         products#destroy
+#          orders GET    /orders(.:format)               orders#index
+#                 POST   /orders(.:format)               orders#create
+#       new_order GET    /orders/new(.:format)           orders#new
+#      edit_order GET    /orders/:id/edit(.:format)      orders#edit
+#           order GET    /orders/:id(.:format)           orders#show
+#                 PATCH  /orders/:id(.:format)           orders#update
+#                 PUT    /orders/:id(.:format)           orders#update
+#                 DELETE /orders/:id(.:format)           orders#destroy
+#     order_items GET    /order_items(.:format)          order_items#index
+#                 POST   /order_items(.:format)          order_items#create
+#  new_order_item GET    /order_items/new(.:format)      order_items#new
+# edit_order_item GET    /order_items/:id/edit(.:format) order_items#edit
+#      order_item GET    /order_items/:id(.:format)      order_items#show
+#                 PATCH  /order_items/:id(.:format)      order_items#update
+#                 PUT    /order_items/:id(.:format)      order_items#update
+#                 DELETE /order_items/:id(.:format)      order_items#destroy
+#           pages GET    /pages(.:format)                pages#index
+#                 POST   /pages(.:format)                pages#create
+#        new_page GET    /pages/new(.:format)            pages#new
+#       edit_page GET    /pages/:id/edit(.:format)       pages#edit
+#            page GET    /pages/:id(.:format)            pages#show
+#                 PATCH  /pages/:id(.:format)            pages#update
+#                 PUT    /pages/:id(.:format)            pages#update
+#                 DELETE /pages/:id(.:format)            pages#destroy
+#           about GET    /about(.:format)                pages#about
+#             faq GET    /faq(.:format)                  pages#faq
+#            blog GET    /blog(.:format)                 pages#blog
+#         contact GET    /contact(.:format)              pages#contact
 #
 
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  resources :categories
+  resources :customers
+  resources :products
+  resources :orders
+  resources :order_items
+  resources :pages
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  get '/about' => 'pages#about'
+  get '/faq' => 'pages#faq'
+  get '/blog' => 'pages#blog'
+  get '/contact' => 'pages#contact'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
